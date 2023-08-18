8fdc864cf6b73f2e78c8ff7417b7120ff4eb0dc4
Purna-ASU <ppeddire@asu.edu>
2023-08-17 18:45:04 -0700

    Select Boxes

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,10 @@ function App() {
     email: "",
     comments: "",
     isFriendly: true,
-    employment: ""
+    employment: "",
+    favColor: ""
   })
-  console.log(formData)
+  console.log(formData.favColor)
   function handleChange(event) {
     const {name, value, type, checked} = event.target
     setFormData(prevFormData => {
@@ -43,6 +44,18 @@ function App() {
         <label htmlFor='full-time'>Full Time</label>
         <br/>
       </fieldset>
+      <label htmlFor="favColor">What is your favorite color?</label>
+            <br />
+            <select id="favColor" onChange={handleChange} name='favColor' value={formData.favColor}>
+                <option value="">-- Choose --</option>
+                <option value="red">Red</option>
+                <option value="orange">Orange</option>
+                <option value="yellow">Yellow</option>
+                <option value="green">Green</option>
+                <option value="blue">Blue</option>
+                <option value="indigo">Indigo</option>
+                <option value="violet">Violet</option>
+            </select>
     </form>
   )
 }
