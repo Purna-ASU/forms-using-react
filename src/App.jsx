ba66e9c25aa35d175eb30c717161cef41cb077b3
Purna-ASU <ppeddire@asu.edu>
2023-08-17 17:41:07 -0700

    Included Checkbox

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,30 @@ function App() {
 
   const [formData, setFormData] = useState({
     firstName: "",
-    lastName: ""
+    lastName: "",
+    email: "",
+    comments: "",
+    isFriendly: true
   })
   console.log(formData)
   function handleChange(event) {
+    const {name, value, type, checked} = event.target
     setFormData(prevFormData => {
       return{
         ...prevFormData,
-        [event.target.name]: event.target.value
+        [name]: type === "checkbox" ? checked : value
       }
     })
   }
 
   return (
     <form>
-      <input type='text' placeholder='FirstName' onChange={handleChange} name='firstName'></input>
-      <input type='text' placeholder='LastName' onChange={handleChange} name='lastName'></input>
+      <input type='text' placeholder='FirstName' onChange={handleChange} name='firstName' value={formData.firstName}></input>
+      <input type='text' placeholder='LastName' onChange={handleChange} name='lastName' value={formData.lastName}></input>
+      <input type='email' placeholder='Email' onChange={handleChange} name='email' value={formData.email}></input>
+      <textarea placeholder='Comments' onChange={handleChange} name='comments' value={formData.comments}/>
+      <input type='checkbox' onChange={handleChange} name='isFriendly' id='isFriendly' checked={formData.isFriendly} />
+      <label htmlFor='isFriendly'>is Friendly?</label>
     </form>
   )
 }
